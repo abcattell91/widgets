36f87396ddbecb1973f6baddf1e64e92354b2cdc
Alex Cattell <abcattell91@gmail.com>
2022-08-07 17:56:32 +0100

    practicing using the active index state to see the clicked index of the item

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
-    return <div key={item.title}>
-      <div className="title active">
-        <i className="dropdown icon"></i>
-        {item.title}
-      </div>
-      <div className="content active">
-        <p>{item.content}</p>
-      </div>
-    </div>
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
   })
   return <div className="ui styled accordion">
     {renderedItems}
+    <h1>{activeIndex}</h1>
   </div>
 };
 
