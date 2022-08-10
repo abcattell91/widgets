af4106a5fd2c27bccb8c71e3e5f056b7596f4129
Alex Cattell <abcattell91@gmail.com>
2022-08-10 16:37:56 +0100

    setting up debounced term to avoid error with dependency

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,15 @@ const Search = () => {
   const [debouncedTerm, setDebouncedTerm] = useState(term)
   const [results, setResults] = useState([]);
 
-  console.log(results)
+  useEffect(() => {
+    const timerId = setTimeout(() =>{
+      setDebouncedTerm(term);
+    }, 650);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -16,25 +24,13 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 650)
-      return() => {
-        clearTimeout(timeoutId);
-      };
-    }
-  },[term, results.length]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result ) => {
     return (
