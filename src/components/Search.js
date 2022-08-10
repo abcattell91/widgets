1e9071451f153151ad711b1ce61bf430d8c371a6
Alex Cattell <abcattell91@gmail.com>
2022-08-10 15:32:31 +0100

    added link to full wiki article

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,6 +27,16 @@ const Search = () => {
   const renderedResults = results.map((result ) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            GO!
+          </a>
+        </div>
         <div className="content">
           <div className="header">
             {result.title}
