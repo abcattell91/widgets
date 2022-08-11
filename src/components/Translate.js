073c3f9814d39c8e069b039cf2c818791d516889
Alex Cattell <abcattell91@gmail.com>
2022-08-11 16:51:18 +0100

    Added convert function into style

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, {useState} from 'react';
-import Dropdown from './Dropdown'
+import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
   {
@@ -50,6 +53,9 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
