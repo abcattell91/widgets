b2b88f006ce515fc0172ebad9e80fd6fc1f751b9
Alex Cattell <abcattell91@gmail.com>
2022-08-16 13:01:45 +0100

    Created separate component for routing instead of multiple lines of similar code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate'
+import Route from './components/Route'
 
 const items = [
   {
@@ -58,11 +59,29 @@ const showTranslate = () => {
   }
 };
 
+
+
 export default () => {
-  return <div>
-    {showAccordion()}
-    {showList()}
-    {showDropdown()}
-    {showTranslate()}
-  </div>;
+  const [selected, setSelected] = useState(options[0])
+  return (
+    <div>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label ="Select a Colour"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
