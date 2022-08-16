305f57c3c6625deeb1c4ef2d8bf230464d6a824f
Alex Cattell <abcattell91@gmail.com>
2022-08-16 13:34:54 +0100

    added event listeners to reduce loading times for pages

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({}, '', href);
+    // this tells the browser url has changed
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
