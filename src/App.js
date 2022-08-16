85b88133f9238da3cd6e12478073648a31ea423b
Alex Cattell <abcattell91@gmail.com>
2022-08-16 12:33:41 +0100

    adding some basic routing

--- src/App.js
+++ src/App.js
@@ -34,11 +34,35 @@ const options = [
   }
 ];
 
-export default () => {
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />
+  }
+};
 
-  return (
-    <div>
-      <Translate />
-    </div>
-  );
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />
+  }
+};
+
+export default () => {
+  return <div>
+    {showAccordion()}
+    {showList()}
+    {showDropdown()}
+    {showTranslate()}
+  </div>;
 };
